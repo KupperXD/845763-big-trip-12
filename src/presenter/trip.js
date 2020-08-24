ffa5ecd5dc266718759244cd8555f5e2a25a5cac
Dmitriy Tarasenko <tarasenko@internet-design.ru>
2020-08-24 18:55:37 +0300

    Доработки

--- src/presenter/trip.js
+++ src/presenter/trip.js
@@ -1,8 +1,19 @@
-
+import {renderElement} from "../utils/render";
+import EventView from "../view/event-item";
+import EventEditView from "../view/event-edit";
+import EventDetailView from "../view/events-detail";
+import FieldsView from "../view/fields-edit";
+import OffersListView from "../view/offers-list";
 
 
 export default class Trip {
   constructor(tripContainer) {
     this._tripContainer = tripContainer;
+
+    init(wayPoints) {
+
+      this._wayPoints = wayPoints.slice();
+
+    }
   }
 }
